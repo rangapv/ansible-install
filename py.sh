48d7a81ea2b6bc236babaf0c79f29ba2c233cdcf
rangapv <rangapv@yahoo.com>
2021-08-13 10:12:57 +0000

    Added some elegency

--- py.sh
+++ py.sh
@@ -302,6 +302,20 @@ fi
 
 }
 
+pyvercheck() {
+args2="$@"
+
+piver=$(${args2[@]} -V 2>&1)
+piver1=$( echo "${piver}" | awk '{split($0,a," ");print a[2]}') ## Strips the python prefix eg... 3.9.4 etc
+piver12=$( echo "${piver1}" | awk '{split($0,a,".");print a[1]}') ## Strips the version suffix leaving only the higer order version 2/3 etc.."
+piver112=$( echo "${piver1}" | awk '{split($0,a,"."); for (i=1; i<2 ; i++) print a[i]"."a[i+1]; }') ## Leaves the first two vrsion info and strips the last number eg...3.9
+piver33=$( echo "${piver}" | awk '{split($0,a,".");print a[2]}') ## Dispalys the middle version number 9 in Python 3.9.4
+
+}
+
+
+# Function declaration ends
+
 if [ $(echo "$li" | grep Linux) ]
 then
   mac=""
@@ -312,7 +326,19 @@ fi
 
 if [[ ( "$#" -eq 0 ) ]]
 then
-	set -- "3.8.7" "Python-3.8.7.tgz"
+	#set -- "3.10.0" "Python-3.10.0a6.tgz"
+	set -- "3.9.4" "Python-3.9.4.tgz"
+	#set -- "3.8.7" "Python-3.8.7.tgz"
+	#set -- "3.7.9" "Python-3.7.9.tgz"
+	#set -- "3.6.12" "Python-3.6.12.tgz" 
+
+   pyvercheck python3
+
+   if [[ ( $piver1 -eq $1 ) ]]
+   then
+      echo "Requirement Satisifed nothing to upgrade or install"
+      exit
+   fi
 fi
 
 
