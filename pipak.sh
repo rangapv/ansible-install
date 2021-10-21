39f71ddf021a4e48fc77793591aff58836eaa53b
rangapv <rangapv@yahoo.com>
2021-10-21 12:01:44 +0000

    Added spacy and nltk packages

--- pipak.sh
+++ pipak.sh
@@ -5,6 +5,16 @@ pip_packages() {
    pip install requests pandas bs4 lxml matplotlib sklearn numpy tensorflow seaborn torch
    pip install gsheetsdb streamlit
    pip install pyOpenSSL
+   
+   #Install spacy
+   pip install spacy
+   python -m spacy download en_core_web_sm
+
+   #Install nltk
+  # pip install nltk
+  # python -m nltk.downloader stopwords
+  # python -m nltk.downloader punkt
+
 }
 
 pip_packages
